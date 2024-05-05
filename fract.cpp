cd3962bca49bf190544cdd471bf3b366a3ed1800
Ar57m <132871733+Ar57m@users.noreply.github.com>
2024-05-05 02:44:52 -0300

    faster and more reliable
    there's a lot to do yet

--- fract.cpp
+++ fract.cpp
@@ -2,8 +2,10 @@
 #include <omp.h>
 #include <math.h>
 #include <cstdint>
-#include <limits>
-#include <iostream>
+
+#include <cmath>
+#include <vector>
+
 
 extern "C" {
 
@@ -12,23 +14,35 @@ extern "C" {
 
         #pragma omp parallel for schedule(dynamic)
         for (uint16_t x = 0; x < width; ++x) {
-            double r_part = xmin + x * dx;
+
+            double r_part = 0;
+            
+            r_part = xmin + x * dx;
+
             for (uint16_t y = 0; y < height; ++y) {
+                
                 double i_part = ymin + y * dy;
                 double z_real = 0;
                 double z_imag = 0;
-                double c_real = r_part;
-                double c_imag = i_part;
+
+                double c_real = 0;
+                double c_imag = 0;
+                c_real = r_part;
+                c_imag = i_part;
                 uint16_t iteration = 0;
+
+
                 while (z_real * z_real + z_imag * z_imag < 4 && iteration < max_iter) {
                     double temp = z_real * z_real - z_imag * z_imag + c_real;
                     z_imag = 2 * z_real * z_imag + c_imag;
+                    z_real = 0;
                     z_real = temp;
                     ++iteration;
                 }
                 output[y * width + x] = iteration;
             }
         }
+
     }
 
 }
@@ -40,15 +54,23 @@ extern "C" {
         
         #pragma omp parallel for schedule(dynamic)
         for (uint16_t x = 0; x < width; ++x) {
-            double r_part = xmin + x * dx;
+            double r_part = 0; 
+            r_part = xmin + x * dx;
+
             for (uint16_t y = 0; y < height; ++y) {
                 double i_part = ymin + y * dy;
-                double z_real = r_part;
-                double z_imag = i_part;
+
+                double z_real = 0;
+                double z_imag = 0;
+                z_real = r_part;
+                z_imag = i_part;
                 uint16_t iteration = 0;
+
+
                 while (z_real * z_real + z_imag * z_imag < 4 && iteration < max_iter) {
                     double temp = z_real * z_real - z_imag * z_imag + c_real;
                     z_imag = 2 * z_real * z_imag + c_imag;
+                    z_real = 0;
                     z_real = temp;
                     ++iteration;
                 }
@@ -57,4 +79,28 @@ extern "C" {
         }
     }
 
+}
+
+
+
+
+extern "C" {
+    void process_array(uint32_t* input_array, uint8_t* output_array, uint16_t width, uint16_t height, double max_value, uint16_t batch_size, double npmax) {
+        // Iterate over each batch
+        for(int i = 0; i < width * height; i += batch_size) {
+            // Iterate over each value in the batch
+            for(int j = i; j < i + batch_size && j < width * height; j++) {
+                // Convert the value to double and scale it
+                double value = (static_cast<double>((input_array[j])) / npmax) * max_value;
+
+                // Round to nearest integer
+                uint32_t rounded_value = static_cast<uint32_t>(std::round(value));
+                
+                // Separate RGB channels
+                output_array[j * 3] = (rounded_value >> 16) & 0xFF;
+                output_array[j * 3 + 1] = (rounded_value >> 8) & 0xFF;
+                output_array[j * 3 + 2] = rounded_value & 0xFF;
+            }
+        }
+    }
 }
--- runner.py
+++ runner.py
@@ -1,10 +1,10 @@
 import numpy as np
-from PIL import Image
+#from PIL import Image
 #%pip install cython
 #import cython
-#import pybind11
 
-from ctypes import cdll, c_int, c_double, POINTER, c_uint64, c_uint32, c_uint16
+import cv2
+from ctypes import cdll, c_double, POINTER, c_uint32, c_uint16, c_uint8
 
 
 
@@ -14,6 +14,12 @@ lib = cdll.LoadLibrary('./libfract.so')
 
 fractal = lib.fractal
 juliaset = lib.juliaset
+
+
+lib.process_array.argtypes = [POINTER(c_uint32), POINTER(c_uint8), c_uint16, c_uint16, c_double, c_uint16, c_double]
+lib.process_array.restype = None
+
+
 fractal.argtypes = [POINTER(c_uint16), c_uint16, c_uint16, c_uint16, c_double, c_double, c_double, c_double]
 juliaset.argtypes = [POINTER(c_uint16), c_uint16, c_uint16, c_uint16, c_double, c_double, c_double, c_double, c_double, c_double]
 
@@ -28,45 +34,36 @@ def scale( input_tensor, new_min, new_max):
         return scaled_tensor
 
 
-def tensor_to_image(tensor, imgname, newmin= None, newmax= None):
-        shape = tensor.shape
-        array_image = np.nan_to_num(tensor.copy(), nan=0, posinf=1, neginf=-1)
-        del tensor
-        if (newmin == None) or (newmax == None):
-            array_image = scale(array_image, 0, 16777215)
-        else:
-            array_image = scale(array_image, newmin, newmax)
-        array_image = np.round(array_image).astype(np.int64)
-        hexa =None
 
-        hexa = array_image
-        del array_image
+
+# This can only scale positive numbers not negatives
+def process_image(input_array, max_val, imgname):
+    width, height = input_array.shape
+    max = np.float64(np.max(input_array))
+    max_val = np.float64(max_val)
     
-        hexa_flat = hexa.reshape(-1)
-        del hexa
-        cor_rgb_flat = np.zeros((hexa_flat.size, 3), dtype=np.int64)
-        cor_rgb_flat[:, 0] = (hexa_flat >> 16) & 0xFF
-        cor_rgb_flat[:, 1] = (hexa_flat >> 8) & 0xFF
-        cor_rgb_flat[:, 2] = hexa_flat & 0xFF
-      
-        del hexa_flat
+    input_array = input_array.copy().reshape(-1).astype(np.uint32)
+    input_array = input_array.ctypes.data_as(POINTER(c_uint32))
+    output_array = (c_uint8 * (width * height* 3))()
+    # Call the function
+    lib.process_array(input_array, output_array, width, height, max_val, 5000, max)
+    # Convert the output array to a numpy array
+    output_array = np.ctypeslib.as_array(output_array).reshape(width, height, 3 )
     
-        cor_rgb = cor_rgb_flat.reshape(shape[0], shape[1], 3).astype(np.uint8)
-        del cor_rgb_flat 
-        #oo = np.full((sh[0], sh[1], 1), 255, dtype=np.uint8)
-        #cor_rgb = np.concatenate((cor_rgb, oo), axis=2)
+    output_image = cv2.cvtColor(output_array, cv2.COLOR_BGR2RGB) 
     
-        imag = Image.fromarray(cor_rgb)
-        del cor_rgb
-        imag.save(f'{imgname}.png')
-        print(f'{imgname}.png' )
+    del output_array
+    cv2.imwrite(f'{imgname}.png', output_image) 
+    print(f'{imgname}.png' )
+
+
+
 
 
 def image_to_array(image_path, min=0, max=2**24-1):
-        img = Image.open(image_path)
-    
+        img = cv2.imread(image_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
         array_image = np.array(img).astype(np.int64)
-    
     
         if array_image.ndim != 3:
             array_image = np.array(img.convert("RGBA"))
@@ -86,9 +83,9 @@ def image_to_array(image_path, min=0, max=2**24-1):
         return array_image
 
 
-
+# Image with palette
 def create_image(palette, data, filename, top_colors=4):
-    data = data.copy() 
+    data = data.copy()
     shape = data.shape
     palette = image_to_array(palette)
     unique_colors, counts = np.unique(palette, return_counts=True)
@@ -96,12 +93,22 @@ def create_image(palette, data, filename, top_colors=4):
     
     array_top_colors = unique_colors[sorted_indices][:top_colors]
 
-
-    data = np.round(scale((np.sin(data)) , 0, array_top_colors.shape[0]-1)).reshape(-1)
+    
+    data = np.round(scale((np.sin(data.astype(np.float64))) , 0, array_top_colors.shape[0]-1)).reshape(-1).astype(np.uint32)
     for i, n in enumerate(array_top_colors):
         data[data == i] = n
+    process_image(data.reshape(shape), np.float64(np.max(data)), filename )
+ 
 
-    tensor_to_image(data.reshape(shape), filename, np.min(data),np.max(data))
+# Not working properly
+def depth_to_intensity(rgb_image, depth_map):
+    depth_map = depth_map.astype(np.float64)
+    normalized_depth = (depth_map / np.max(depth_map))
+    
+    intensity_image = rgb_image.astype(np.float64) * normalized_depth.reshape(-1) #[:,np.newaxis]
+    
+    return np.round(intensity_image).astype(np.uint8)
+
 
 
 
@@ -114,6 +121,8 @@ xmin, xmax = -16/6, 16/6   #-16/5, 16/5
 ymin, ymax = -16/6, 16/6   #-9/5, 9/5
 
 max_iter = 1000
+
+# How many top colors to use from the palette.png
 top_colors = 6
 
 juliaset_c_real = -0.8
@@ -123,6 +132,8 @@ juliaset_c_imag = 0.16
 
 
 import time
+
+# Mandelbrot Set
 start_time = time.perf_counter()
 
 mandelbrot_set = np.empty((height, width), dtype=np.uint16)
@@ -131,14 +142,11 @@ fractal(mandelbrot_set.ctypes.data_as(POINTER(c_uint16)), width, height, max_ite
 end_time = time.perf_counter()
 
 print("Took ", end_time - start_time, "seconds to generate")
+process_image(mandelbrot_set, (2**24-1), "generated_fractal" )
+create_image("palette.png",mandelbrot_set.reshape(width, height), "colorful", top_colors=top_colors)
 
 
-create_image("palette.png",mandelbrot_set, "colorful", top_colors=top_colors)
-tensor_to_image(mandelbrot_set,"generated_fractal", 0,2**24-1)
-
-
-
-
+# Julia Set
 start_time = time.perf_counter()
 
 julia_set = np.empty((height, width), dtype=np.uint16)
@@ -147,12 +155,8 @@ juliaset(julia_set.ctypes.data_as(POINTER(c_uint16)), width, height, max_iter, j
 end_time = time.perf_counter()
 
 print("Took ", end_time - start_time, "seconds to generate")
-
-
-create_image("palette.png",julia_set, "colorful_julia_set", top_colors=top_colors)
-tensor_to_image(julia_set, "generated_fractal_julia_set", 0,2**24-1)
-
-
+process_image(julia_set, (2**24-1), "generated_fractal_julia_set" )
+create_image("palette.png",julia_set.reshape(width, height), "colorful_julia_set", top_colors=top_colors)
 
 
 
